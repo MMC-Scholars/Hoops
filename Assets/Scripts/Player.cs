52d7ca38a5a37dd8c10c372f0a6c0b03a4c883d8
Keith Kriston <59848719+Kriston5@users.noreply.github.com>
2020-09-22 16:58:43 -0400

    Made it so basketballs can be picked up and set down.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour, IBaseEntity
+{
+    private RaycastHit lookingAt;
+    private GameObject heldObject;
+    private float holdDistance;
+    public float lookDistance = 10.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        raycastUpdate();
+        
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+
+    void raycastUpdate()
+    {
+        if(Physics.Raycast(this.transform.position, this.transform.TransformDirection(Vector3.forward), out lookingAt, lookDistance))
+        {
+            Debug.Log("Looking at: " + lookingAt.collider.gameObject.name);
+            
+            pickUp();
+        }
+    }
+    void pickUp()
+    {
+        if (lookingAt.collider.gameObject.CompareTag("Basketball"))
+        {
+            if(Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                heldObject = lookingAt.collider.gameObject;
+                holdDistance = lookingAt.distance;
+            }
+            else if (Input.GetKeyUp(KeyCode.Mouse0))
+            {
+                heldObject = null;
+            }
+        }
+
+        holdObject();
+    }
+
+    void holdObject()
+    {
+        if(heldObject != null)
+        {
+            heldObject.transform.position = this.transform.position + holdDistance * this.transform.TransformDirection(Vector3.forward);
+        }
+    }
+}
