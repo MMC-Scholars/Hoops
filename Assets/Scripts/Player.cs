9db5518d9032675b941ae226bed2b7d28bb290fe
Sam Bossley <35381535+bossley9@users.noreply.github.com>
2020-09-28 14:57:54 -0400

    Update Player script with BaseUpdate method

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,66 +1,51 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Player : MonoBehaviour, IBaseEntity
-{
+public class Player : BasePawn {
     private RaycastHit lookingAt;
     private GameObject heldObject;
-    private float holdDistance;
-    public float interactDistance = 10.0f;
-    public float throwStrength = 10.0f;
+    private float      holdDistance;
+    public float       interactDistance = 10.0f;
+    public float       throwStrength    = 10.0f;
 
+    /**
+     * Called on each update frame
+     */
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public override void BaseUpdate() {
+        raycastUpdate();
+        if (Input.GetKeyDown(KeyCode.E)) pickUp();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        raycastUpdate(); 
-        if(Input.GetKeyDown(KeyCode.E)) pickUp();
-    }
+    void FixedUpdate() {}
 
-    void FixedUpdate()
-    {
-
-    }
-
-
-    void raycastUpdate()
-    {
-        if(Physics.Raycast(this.transform.position, this.transform.TransformDirection(Vector3.forward), out lookingAt, interactDistance))
-        {
+    void raycastUpdate() {
+        if (Physics.Raycast(this.transform.position,
+                            this.transform.TransformDirection(Vector3.forward),
+                            out lookingAt, interactDistance)) {
             Debug.Log("Looking at: " + lookingAt.collider.gameObject.name);
-            
-        }
-        else 
-        {
+
+        } else {
             Debug.Log("Looking at: nothing");
         }
     }
-    void pickUp()
-    {
-        //If currently holding something, drop it
-        if(heldObject != null)
-        {
+    void pickUp() {
+        // If currently holding something, drop it
+        if (heldObject != null) {
             heldObject.transform.SetParent(null);
             heldObject.GetComponent<Rigidbody>().useGravity = true;
-            heldObject.GetComponent<Rigidbody>().AddForce(throwStrength * this.transform.TransformDirection(Vector3.forward), ForceMode.Impulse);
+            heldObject.GetComponent<Rigidbody>().AddForce(
+                throwStrength * this.transform.TransformDirection(Vector3.forward),
+                ForceMode.Impulse);
             Physics.Simulate(Time.fixedDeltaTime);
             heldObject = null;
         }
-        //If looking at a basketball, pick it up
-        else if(lookingAt.collider.gameObject.CompareTag("Basketball"))
-        {
+        // If looking at a basketball, pick it up
+        else if (lookingAt.collider.gameObject.CompareTag("Basketball")) {
             heldObject = lookingAt.collider.gameObject;
-            heldObject.GetComponent<Rigidbody>().useGravity = false;
-            heldObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            heldObject.GetComponent<Rigidbody>().useGravity      = false;
+            heldObject.GetComponent<Rigidbody>().velocity        = Vector3.zero;
             heldObject.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
             heldObject.transform.SetParent(this.transform);
-        } 
+        }
     }
 }
