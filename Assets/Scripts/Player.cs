e3211c693e1d3e955272ab68a615c5116bff17e5
Keith Kriston <59848719+Kriston5@users.noreply.github.com>
2020-09-29 16:19:06 -0400

    Throw strength is determined by the length of a key press.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,9 +3,10 @@
 public class Player : BasePawn {
     private RaycastHit lookingAt;
     private GameObject heldObject;
-    private float      holdDistance;
+    private float      throwStrength    = 0.0f;
     public float       interactDistance = 10.0f;
-    public float       throwStrength    = 10.0f;
+    public float       maxThrow         = 10.0f;
+    public float       throwModifier    = 1.0f;
 
     /**
      * Called on each update frame
@@ -13,7 +14,8 @@ public class Player : BasePawn {
 
     public override void BaseUpdate() {
         raycastUpdate();
-        if (Input.GetKeyDown(KeyCode.E)) pickUp();
+        if (Input.GetKeyDown(KeyCode.Mouse0)) pickUp();
+        if (heldObject) throwObject();
     }
 
     void FixedUpdate() {}
@@ -29,23 +31,28 @@ public class Player : BasePawn {
         }
     }
     void pickUp() {
-        // If currently holding something, drop it
-        if (heldObject != null) {
-            heldObject.transform.SetParent(null);
-            heldObject.GetComponent<Rigidbody>().useGravity = true;
-            heldObject.GetComponent<Rigidbody>().AddForce(
-                throwStrength * this.transform.TransformDirection(Vector3.forward),
-                ForceMode.Impulse);
-            Physics.Simulate(Time.fixedDeltaTime);
-            heldObject = null;
-        }
-        // If looking at a basketball, pick it up
-        else if (lookingAt.collider.gameObject.CompareTag("Basketball")) {
+        if (lookingAt.normal != Vector3.zero && lookingAt.collider.gameObject.CompareTag("Basketball")) {
             heldObject = lookingAt.collider.gameObject;
             heldObject.GetComponent<Rigidbody>().useGravity      = false;
             heldObject.GetComponent<Rigidbody>().velocity        = Vector3.zero;
             heldObject.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
             heldObject.transform.SetParent(this.transform);
+        }
+    }
+    void throwObject() {
+
+        if(Input.GetKey(KeyCode.Mouse1)){
+            throwStrength += throwModifier * Time.deltaTime;
+            if(throwStrength > maxThrow) throwStrength = maxThrow;
+        } 
+        else if (Input.GetKeyUp(KeyCode.Mouse1)){
+        heldObject.transform.SetParent(null);
+        heldObject.GetComponent<Rigidbody>().useGravity = true;
+        heldObject.GetComponent<Rigidbody>().AddForce(
+            throwStrength * this.transform.TransformDirection(Vector3.forward),
+            ForceMode.Impulse);
+        heldObject    = null;
+        throwStrength = 0.0f;
         }
     }
 }
