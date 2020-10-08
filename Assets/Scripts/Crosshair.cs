d147b7fe1ba4168eb08d2785ca13baac4224ab8c
Keith Kriston <59848719+Kriston5@users.noreply.github.com>
2020-10-08 16:14:27 -0400

    Removed Update from Player.cs and made font size a public variable.

--- Assets/Scripts/Crosshair.cs
+++ Assets/Scripts/Crosshair.cs
@@ -5,25 +5,20 @@ using UnityEngine.UI;
 
 public class Crosshair : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Start is called before the first frame
+    public int size = 48;
     void Start()
     {
         GameObject m_crosshair = new GameObject("Crosshair", typeof(Text));
         m_crosshair.transform.SetParent(this.transform);
-        m_crosshair.GetComponent<RectTransform>().anchorMin         = new Vector2(0.5f, 0.5f);
-        m_crosshair.GetComponent<RectTransform>().anchorMax         = new Vector2(0.5f, 0.5f);
-        m_crosshair.GetComponent<RectTransform>().anchoredPosition  = Vector2.zero;
-        m_crosshair.GetComponent<Text>().fontSize                   = 48;
-        m_crosshair.GetComponent<Text>().font                       = Resources.GetBuiltinResource<Font>("Arial.ttf");
-        m_crosshair.GetComponent<Text>().color                      = Color.black;
-        m_crosshair.GetComponent<Text>().alignment                  = TextAnchor.MiddleCenter;
-        m_crosshair.GetComponent<Text>().text                       = "+";
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        m_crosshair.GetComponent<RectTransform>().anchorMin = new Vector2(0.5f, 0.5f);
+        m_crosshair.GetComponent<RectTransform>().anchorMax = new Vector2(0.5f, 0.5f);
+        m_crosshair.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+        m_crosshair.GetComponent<Text>().fontSize = size;
+        m_crosshair.GetComponent<Text>().font = Resources.GetBuiltinResource<Font>("Arial.ttf");
+        //TODO make the crosshair contrast all environments
+        m_crosshair.GetComponent<Text>().color = Color.black;
+        m_crosshair.GetComponent<Text>().alignment = TextAnchor.MiddleCenter;
+        m_crosshair.GetComponent<Text>().text = "+";
     }
 }
