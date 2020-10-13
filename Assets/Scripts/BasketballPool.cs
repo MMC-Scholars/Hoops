32b8396e4a26cca1e8e993d6a081999be60313e7
Keith Kriston <59848719+Kriston5@users.noreply.github.com>
2020-10-13 16:46:29 -0400

    Made the player automatically pick up basketballs

---
+++ Assets/Scripts/BasketballPool.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasketballPool : MonoBehaviour
+{
+    public List<GameObject> pool;
+    public int size = 5;
+    public GameObject basketball;
+    private int next = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < size; i++)
+        {
+            GameObject obj = (GameObject)Instantiate(basketball);
+            obj.SetActive(false);
+            pool.Add(obj);
+        }
+    }
+
+    public GameObject getObject()
+    {
+        return pool[next++ % size];
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,23 +1,27 @@
 ﻿using UnityEngine;
 
-public class Player : BasePawn {
-    private RaycastHit lookingAt;
+public class Player : BasePawn
+{
     private GameObject heldObject;
-    public float      throwStrength    = 0.0f;
-    public float       interactDistance = 10.0f;
-    public float       maxThrow         = 10.0f;
-    public float       throwModifier    = 1.0f;
+    private float cooldown = 2.0f;
+    private bool isThrowing = false;
 
+    public float throwStrength = 0.0f;
+    public float interactDistance = 10.0f;
+    public float maxThrow = 10.0f;
+    public float throwModifier = 1.0f;
+    public float reloadTime = 2.0f;
     /**
      * Called on each update frame to update input
      */
 
-    public override void BaseUpdateInput() {
-        if (BaseInput.isTriggerPressed()) pickUp();
-        if (heldObject){
-            //TODO change inputs to the equivalent BaseInput 
-            if(Input.GetKey(KeyCode.Mouse1)) startThrow();
-            else if (Input.GetKeyUp(KeyCode.Mouse1)) throwObject();
+    public override void BaseUpdateInput()
+    {
+        isThrowing = (BaseInput.isTriggerPressed() || (isThrowing && !BaseInput.isTriggerReleased()));
+        if (heldObject)
+        {
+            if (isThrowing) startThrow();
+            if (BaseInput.isTriggerReleased()) throwObject();
         }
     }
 
@@ -25,30 +29,39 @@ public class Player : BasePawn {
      * Called on each update frame
      */
 
-    public override void BaseUpdate() { 
-        Physics.Raycast(this.transform.position, this.transform.TransformDirection(Vector3.forward),
-                        out lookingAt, interactDistance);
+    public override void BaseUpdate()
+    {
+        if (!heldObject) pickUp();
     }
-    void pickUp() {
-        if (lookingAt.normal != Vector3.zero && lookingAt.collider.gameObject.CompareTag("Basketball")) {
-            heldObject = lookingAt.collider.gameObject;
-            heldObject.GetComponent<Rigidbody>().useGravity      = false;
-            heldObject.GetComponent<Rigidbody>().velocity        = Vector3.zero;
+    void pickUp()
+    {
+        cooldown -= Time.deltaTime;
+        if (cooldown <= 0)
+        {
+            heldObject = this.GetComponent<BasketballPool>().getObject();
+            heldObject.SetActive(true);
+            heldObject.GetComponent<Rigidbody>().useGravity = false;
+            heldObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
             heldObject.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
             heldObject.transform.SetParent(this.transform);
+            heldObject.transform.SetPositionAndRotation(this.transform.TransformDirection(Vector3.forward) * interactDistance, Quaternion.identity);
+            cooldown = reloadTime;
         }
-}
-    void startThrow() {
-        throwStrength += throwModifier * Time.deltaTime;
-        if(throwStrength > maxThrow) throwStrength = maxThrow;
     }
-    void throwObject() {
-    heldObject.transform.SetParent(null);
-    heldObject.GetComponent<Rigidbody>().useGravity = true;
-    heldObject.GetComponent<Rigidbody>().AddForce(
-        throwStrength * this.transform.TransformDirection(Vector3.forward),
-        ForceMode.Impulse);
-    heldObject    = null;
-    throwStrength = 0.0f;
+    void startThrow()
+    {
+        throwStrength += throwModifier * Time.deltaTime;
+        if (throwStrength > maxThrow) throwStrength = maxThrow;
+    }
+    void throwObject()
+    {
+        heldObject.transform.SetParent(null);
+        heldObject.GetComponent<Rigidbody>().useGravity = true;
+        heldObject.GetComponent<Rigidbody>().AddForce(
+            throwStrength * this.transform.TransformDirection(Vector3.forward),
+            ForceMode.Impulse);
+        heldObject = null;
+        throwStrength = 0.0f;
+        isThrowing = false;
     }
 }
