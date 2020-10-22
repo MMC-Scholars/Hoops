4626bf63ef2579358e45f229096a998ec02c9437
Keith Kriston <59848719+Kriston5@users.noreply.github.com>
2020-10-22 15:12:09 -0400

    Made hoops orbit the player at different speeds as the game gets harder

--- Assets/Scripts/Hoop.cs
+++ Assets/Scripts/Hoop.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Hoop : ABaseEntity
 {
     public int value = 1;
+    private int currentSpeed;
+    public int[] speeds = { 0, 5, 15, 30, 50 };
+    public Player player;
     public ScoreManager scoreManager;
     void OnTriggerEnter(Collider other)
     {
@@ -12,5 +15,14 @@ public class Hoop : ABaseEntity
         {
             scoreManager.Score += value;
         }
+    }
+    public override void BaseUpdate()
+    {
+        if (currentSpeed != speeds[scoreManager.Difficulty])
+        {
+            currentSpeed = speeds[scoreManager.Difficulty];
+        }
+
+        this.transform.RotateAround(player.transform.position, Vector3.up, currentSpeed * Time.deltaTime);
     }
 }
--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -6,6 +6,9 @@ using UnityEngine.UI;
 public class ScoreManager : ABaseEntity
 {
     private int score = 0;
+    private int difficulty = 0;
+    private int maxDifficulty = 4;
+    private int[] difficultyThresholds = { 10, 25, 50, 100 };
     public int Score
     {
         get => score;
@@ -13,9 +16,17 @@ public class ScoreManager : ABaseEntity
         {
             score = value;
             this.GetComponent<Text>().text = "Score: " + score;
+            if (difficulty < maxDifficulty && score >= difficultyThresholds[difficulty])
+            {
+                Debug.Log("Difficulty up");
+                difficulty++;
+            }
         }
     }
-
+    public int Difficulty
+    {
+        get => difficulty;
+    }
     public override void BaseStart()
     {
         this.GetComponent<Text>().text = "Score: 0";
